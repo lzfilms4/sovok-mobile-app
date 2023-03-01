6bb9603277cb3589ecef418b09c3240ebdcc2dcf
lzfilms5 <lzfilms4@gmail.com>
2023-03-01 19:14:39 +0700

    Update App.js

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dad7cd',
     alignItems: 'center',
     justifyContent: 'center',
   },
